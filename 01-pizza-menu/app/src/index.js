5260896427891599ffe180acb4a6a4cf099604a9
Obua Stella <stellaobua123@gmail.com>
2024-02-13 13:26:12 +0100

    JavaScript Logic in Components

--- 01-pizza-menu/app/src/index.js
+++ 01-pizza-menu/app/src/index.js
@@ -82,6 +82,12 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  console.log(hour);
   return (
     <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
   );
