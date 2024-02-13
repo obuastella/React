03f9abe6b8b85ee9df76888c4ea3dccf32774159
Obua Stella <stellaobua123@gmail.com>
2024-02-13 12:51:32 +0100

    38

--- 01-pizza-menu/app/src/index.js
+++ 01-pizza-menu/app/src/index.js
@@ -49,8 +49,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Pizza />
-      <Pizza />
+      <Menu />
       <Footer />
     </div>
   );
@@ -71,10 +70,22 @@ function Header() {
   return <h1>Fast React Pizza Co.</h1>;
 }
 
-function Menu() {}
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
 
 function Footer() {
-  return React.createElement("footer", null, "We're currently open!");
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
+  );
+  // return React.createElement("footer", null, "We're currently open!");
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
