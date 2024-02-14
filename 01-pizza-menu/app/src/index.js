d2745f1bb90732d2c428c5ba5552e177e86826be
Obua Stella <stellaobua123@gmail.com>
2024-02-14 23:41:03 +0100

    47: Conditional Rendering with &&

--- 01-pizza-menu/app/src/index.js
+++ 01-pizza-menu/app/src/index.js
@@ -65,14 +65,22 @@ function Header() {
 }
 
 function Menu() {
+  // const pizzas = pizzaData;
+  const pizzas = [];
+  const numPizzas = pizzas.length;
+  // An empty array is a falsy value.
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {/* If there are pizzas in the array then it should display pizza. */}
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
+
       {/* <Pizza
         name="/pizzas/spinaci.jpg"
         photoName="Pizza Spinaci"
@@ -103,13 +111,21 @@ function Pizza(props) {
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
-  const closeHour = 22;
+  const closeHour = 24;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
   console.log(hour);
   return (
     <footer className="footer">
-      {isOpen && "We are currently open!"}
+      {isOpen && (
+        <div className="order">
+          <p>
+            We are currently open until {closeHour}:00. Come visit us or order
+            online.
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      )}
       {/* {new Date().toLocaleTimeString()} We're currently open! */}
     </footer>
   );
