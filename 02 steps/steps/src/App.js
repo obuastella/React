011f406c790f881d7fdb87baa46fefc290d94751
Obua Stella <stellaobua123@gmail.com>
2024-02-17 23:10:09 +0100

    58: Event handling

--- 02 steps/steps/src/App.js
+++ 02 steps/steps/src/App.js
@@ -4,6 +4,12 @@ const message = [
   "Invest your new income 🤑",
 ];
 function App() {
+  function handlePrevious() {
+    return alert("Previous");
+  }
+  function handleNext() {
+    return alert("Next");
+  }
   const step = 1;
   return (
     <div className="steps">
@@ -16,10 +22,16 @@ function App() {
         Step {step}: {message[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
