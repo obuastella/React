be6f74ccdad9abda0ab3c56398df747dcde663cf
Obua Stella <stellaobua123@gmail.com>
2024-02-19 20:56:46 +0100

    Added select

--- 03-travel-list/travel-list/src/App.jsx
+++ 03-travel-list/travel-list/src/App.jsx
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 function App() {
   return (
@@ -24,6 +24,15 @@ function Form() {
   return (
     <div className="add-form">
       <h3>What do you need for your 😍 trip? </h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={1} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>ADD</button>
     </div>
   );
 }
@@ -32,7 +41,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
