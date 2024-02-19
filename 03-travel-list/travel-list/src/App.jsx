209163378aad908a35b40265995d2ae5636fa0a4
Obua Stella <stellaobua123@gmail.com>
2024-02-19 20:37:15 +0100

    71: Rendering the items list

--- 03-travel-list/travel-list/src/App.jsx
+++ 03-travel-list/travel-list/src/App.jsx
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
 function App() {
   return (
     <>
@@ -23,8 +28,30 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+        <button>❌</button>
+      </span>
+    </li>
+  );
+}
+// function Item({ item }) {
+//   return <li>{item.description}</li>;
+// }
+
 function Stats() {
   return (
     <footer className="stats">
