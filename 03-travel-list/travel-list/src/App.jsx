b5c009d355f72a9693b3a9414405bb0996fa33b3
Obua Stella <stellaobua123@gmail.com>
2024-02-19 20:06:49 +0100

    Setup component structure

--- 03-travel-list/travel-list/src/App.jsx
+++ 03-travel-list/travel-list/src/App.jsx
@@ -1,5 +1,34 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </>
+  );
 }
 
 export default App;
+
+function Logo() {
+  return <h1>🏝 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip? </h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have x item on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
